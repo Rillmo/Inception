17f6e1cbb52e2fcc3f3f57b43c9e07014fd22ba7
김준호 <junkim2@student.42seoul.kr>
2024-07-20 04:20:28 +0900

    feat: finished wordpress setting

--- srcs/requirements/mariadb/tools/docker-entrypoint.sh
+++ srcs/requirements/mariadb/tools/docker-entrypoint.sh
@@ -2,47 +2,43 @@
 
 set -e
 
-# 외부 ip 허용
-echo "external ip setting"
-sed -i "s|.*bind-address\s*=.*|#bind-address=127.0.0.1|g" /etc/mysql/mariadb.conf.d/50-server.cnf
-echo "complete"
 
-# Check if data directory is empty
 if [ ! -d "/var/lib/mysql/mysql" ]; then
-  echo "Initializing MariaDB data directory..."
-  mysql_install_db --user=mysql --datadir=/var/lib/mysql
+	# 외부 ip 허용
+	echo "external ip setting"
+	sed -i "s|.*bind-address\s*=.*|#bind-address=127.0.0.1|g" /etc/mysql/mariadb.conf.d/50-server.cnf
+	echo "complete"
+	
+	# Check if data directory is empty
+	echo "data dir init"
+	echo "Initializing MariaDB data directory..."
+	mysql_install_db --user=mysql --datadir=/var/lib/mysql
+	echo "complete"
+
+	# mariadb 서버 임시 시작
+	echo "starting temp mariadb server"
+	mariadbd -u root &
+	MARIADB_PID=$!
+	echo "complete"
+
+	sleep 3
+
+	# wordpress용 user & db 생성
+	echo "wordpress user & db setting"
+	mariadb -u root <<-EOSQL
+		CREATE DATABASE IF NOT EXISTS wordpress_db;
+		CREATE USER 'junkim2'@'%' IDENTIFIED BY "$MYSQL_USER_PASSWORD";
+		GRANT ALL ON wordpress_db.* TO 'junkim2'@'%';
+		FLUSH PRIVILEGES;
+		ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
+	EOSQL
+	echo "complete"
+	sleep 3
+	kill "$MARIADB_PID"
+	wait "$MARIADB_PID"
 fi
 
-# mariadb 서버 임시 시작
-echo "starting mariadb"
-mariadbd -u root &
-MARIADB_PID=$!
-echo "complete"
-
-sleep 3
-
-# root 비밀번호 설정
-echo "root password setting"
-mariadb -u root <<-EOSQL
-	ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
-EOSQL
-echo "complete"
-
-# wordpress용 user & db 생성
-echo "wordpress user & db setting"
-mariadb -u root -p$MYSQL_ROOT_PASSWORD <<-EOSQL
-	CREATE DATABASE IF NOT EXISTS wordpress_db;
-	CREATE USER 'junkim2'@'%' IDENTIFIED BY "$MYSQL_USER_PASSWORD";
-	GRANT ALL ON wordpress_db.* TO 'junkim2'@'%';
-	FLUSH PRIVILEGES;
-EOSQL
-echo "complete"
-
-sleep 3
-
 # mariadb 서버 시작
-echo "restarting mariadb"
-kill "$MARIADB_PID"
-wait "$MARIADB_PID"
+echo "starting mariadb server"
 exec mariadbd -u root
 echo "complete"
--- srcs/requirements/wordpress/tools/docker-entrypoint.sh
+++ srcs/requirements/wordpress/tools/docker-entrypoint.sh
@@ -1,25 +1,42 @@
 #!/bin/sh
 
-# wordpress 설정파일 이름 변경
-mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
-
-# wordpress DB 설정
-sed -i "s/database_name_here/'$WORDPRESS_DB'/g" /var/www/html/wp-config.php
-sed -i "s/username_here/'$WORDPRESS_DB_USERNAME'/g" /var/www/html/wp-config.php
-sed -i "s/password_here/'$WORDPRESS_DB_PASSWORD'/g" /var/www/html/wp-config.php
-sed -i "s/localhost/'$WORDPRESS_DB_HOST'/g" /var/www/html/wp-config.php
+set -e
+sleep 10
 
 # PHP 버전 가져오기
+echo "php version check"
 PHP_VERSION=$(ls /etc/php/ | grep -E '^[0-9]+\.[0-9]+$')
+echo "complete"
 
-# php 외부 연결 오픈
-CONFIG_FILE="/etc/php/$PHP_VERSION/fpm/pool.d/www.conf"
-if [ -f "$CONFIG_FILE" ]; then
-    # 'listen' 설정 변경
-    sed -i 's|^listen = /run/php/php'"$PHP_VERSION"'-fpm.sock.*|listen = 0.0.0.0:9000|' "$CONFIG_FILE"
-    echo "Updated $CONFIG_FILE"
-else
-    echo "Config file not found for PHP version $PHP_VERSION"
+if [ -f /var/www/html/wp-config-sample.php ]; then
+    # wordpress 설정파일 이름 변경
+    echo "wp-config setting"
+    mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+    echo "complete"
+
+    # wordpress DB 설정
+    echo "wordpress db setting"
+    sed -i "s/database_name_here/'$WORDPRESS_DB'/g" /var/www/html/wp-config.php
+    sed -i "s/username_here/'$WORDPRESS_DB_USERNAME'/g" /var/www/html/wp-config.php
+    sed -i "s/password_here/'$WORDPRESS_DB_PASSWORD'/g" /var/www/html/wp-config.php
+    sed -i "s/localhost/'$WORDPRESS_DB_HOST'/g" /var/www/html/wp-config.php
+    echo "complete"
+
+
+    # php 외부 연결 오픈
+    echo "php external connection open"
+    CONFIG_FILE="/etc/php/$PHP_VERSION/fpm/pool.d/www.conf"
+    if [ -f "$CONFIG_FILE" ]; then
+        # 'listen' 설정 변경
+        sed -i 's|^listen = /run/php/php'"$PHP_VERSION"'-fpm.sock.*|listen = 0.0.0.0:9000|' "$CONFIG_FILE"
+        echo "Updated $CONFIG_FILE"
+    else
+        echo "Config file not found for PHP version $PHP_VERSION"
+    fi
+    echo "complete"
 fi
 
-exec service php$PHP_VERSION-fpm start
+# php-fpm 시작
+echo "starting php-fpm$PHP_VERSION"
+exec /usr/sbin/php-fpm$PHP_VERSION -F -R
+echo "complete"
