c7b797d88f4e76cdd79e8bd6003fc4cbb82e2d46
김준호 <junkim2@student.42seoul.kr>
2024-07-21 21:44:33 +0900

    feat: finished wordpress auto setting

--- srcs/requirements/wordpress/tools/docker-entrypoint.sh
+++ srcs/requirements/wordpress/tools/docker-entrypoint.sh
@@ -8,16 +8,30 @@ echo "php version check"
 PHP_VERSION=$(ls /etc/php/ | grep -E '^[0-9]+\.[0-9]+$')
 echo "complete"
 
-# wordpress 설치
-echo "installing wordpress"
-wget https://wordpress.org/latest.tar.gz && \
-tar -xvf latest.tar.gz && \
-mv /wordpress/* /var/www/html/ && \
-chown -R www-data:www-data /var/www/html && \
-rm -r /wordpress latest.tar.gz
-echo "complete"
+# if [ ! -f /var/www/html/wp-config.php ]; then
+#     # wp-cli 설치
+#     echo "installing wp-cli"
+#     wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+#     chmod +x wp-cli.phar
+#     mv wp-cli.phar /usr/local/bin/wp
+#     echo "complete"
 
-if [ -f /var/www/html/wp-config-sample.php ]; then
+#     # wp-cli 세팅
+#     wp core download --allow-root --path=/var/www/html
+
+# fi
+
+
+if [ ! -f /var/www/html/wp-config.php ]; then
+    # wordpress 설치
+    echo "installing wordpress"
+    wget https://wordpress.org/latest.tar.gz && \
+    tar -xvf latest.tar.gz && \
+    mv /wordpress/* /var/www/html/ && \
+    chown -R www-data:www-data /var/www/html && \
+    rm -r /wordpress latest.tar.gz
+    echo "complete"
+
     # wordpress 설정파일 이름 변경
     echo "wp-config setting"
     mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
@@ -35,6 +49,27 @@ if [ -f /var/www/html/wp-config-sample.php ]; then
     sed -i "s/username_here/$WORDPRESS_DB_USERNAME/g" /var/www/html/wp-config.php
     sed -i "s/password_here/$WORDPRESS_DB_PASSWORD/g" /var/www/html/wp-config.php
     sed -i "s/localhost/$WORDPRESS_DB_HOST/g" /var/www/html/wp-config.php
+    echo "complete"
+
+    # wp-cli 세팅
+    echo "setting with wp-cli"
+    wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+    chmod +x wp-cli.phar
+    mv wp-cli.phar /usr/local/bin/wp
+    wp core install \
+        --allow-root \
+        --path=/var/www/html \
+        --url=https://localhost \
+        --title=inception \
+        --admin_user=nimda \
+        --admin_password=1234 \
+        --admin_email=nimda@gmail.com
+    wp user create \
+        $WORDPRESS_USER $WORDPRESS_USER_EMAIL \
+        --user_pass=$WORDPRESS_USER_PASSWORD \
+        --role=author \
+        --allow-root \
+        --path=/var/www/html
     echo "complete"
 
     # php 외부 연결 오픈
